c3e94f1c3348da99fdc6a22179bae989fcaa1225
aanchalatre2014 <aanchalatre2014@gmail.com>
2023-01-21 22:51:30 +0530

    Variable1

---
+++ src/Namevariable.java
@@ -0,0 +1,11 @@
+public class Namevariable {
+    public static void main(String[]args){
+        String name= "Aanchal";
+        System.out.println("I am "+name);
+        int age= 42;
+        System.out.println("Age"+age);
+        double salary= 2567.50;
+        System.out.println("Salary is"+salary);
+    }
+}
+
